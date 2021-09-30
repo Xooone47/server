3084e1a60a2168176bdd3810516701d405a1212e
Deland <insidethe47s@qq.com>
2021-09-30 17:23:25 +0800

    init

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const port = 8047;
+const {noop, time} = require('@/utils');
+
+time();
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+});
+
+
---
+++ src/utils/index.ts
@@ -0,0 +1,7 @@
+const {map} = require('lodash');
+const time1 = require('./time');
+
+module.exports = {
+    noop: map,
+    time: time1,
+};
